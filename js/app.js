b229d3976801a3a231a81ce677ae8b447bf53885
Kashish Verma <156228507+KashishV999@users.noreply.github.com>
2024-11-25 17:39:18 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,17 +1,4 @@
-/**
- * WEB222 – Assignment 3
- *
- * I declare that this assignment is my own work in accordance with
- * Seneca Academic Policy. No part of this assignment has been
- * copied manually or electronically from any other source
- * (including web sites) or distributed to other students.
- *
- * Please update the following with your information:
- *
- *      Name:       Kashish Verma
- *      Student ID: 151579232
- *      Date:      14 July 2024
- */
+
 
 // All of our data is available on the global `window` object.
 // Create local variables to work with it in this file.
